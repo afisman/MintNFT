5ed22ed87f90a327b990462434888b0ae8a9f572
afisman <afisman@gmail.com>
2023-05-29 13:28:26 +0200

    succesful mint

--- ipfs/upload.js
+++ ipfs/upload.js
@@ -1,29 +1,30 @@
 async function run() {
     const { create } = await import('ipfs-http-client');
     const ipfs = await create();
-    
+
     // we added three attributes, add as many as you want!
     const metadata = {
         path: '/',
         content: JSON.stringify({
-            name: "My First NFT",
+            name: "ALF",
             attributes: [
-            {
-                "trait_type": "Peace",
-                "value": "10" 
-            },
-            {
-                "trait_type": "Love",
-                "value": "100"
-            },
-            {
-                "trait_type": "Web3",
-                "value": "1000"
-            }
+                {
+                    "trait_type": "Peace",
+                    "value": "10"
+                },
+                {
+                    "trait_type": "Love",
+                    "value": "100"
+                },
+                {
+                    "trait_type": "Web3",
+                    "value": "1000"
+                }
             ],
             // update the IPFS CID to be your image CID
-            image: "https://ipfs.io/ipfs/QmQ2wnwaFJ1w42UTywTWpM8RgiqrWwKFR6AMrpyiHPgi3p",
-            description: "So much PLW3!"
+
+            image: "https://ipfs.io/ipfs/QmVEgw2i9CaJdGhkFsu7KLXubJteAad4RVudzKLfrjBuYp",
+            description: "ALF"
         })
     };
 
